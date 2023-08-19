3ebbe67e65e59365de6c6fb59cd6cac56532ee3b
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-08-19 17:15:23 -0500

    test: predict if pico and placa applies according to time

--- src/predictor.py
+++ src/predictor.py
@@ -1,3 +1,6 @@
 class Predictor:
     def return_last_digit(self, license_plate):
         return int(license_plate[-1])
+
+    def predict(self, str_time):
+        pass
--- test/test_predictor.py
+++ test/test_predictor.py
@@ -7,3 +7,6 @@ class TestPredictor(unittest.TestCase):
 
     def test_return_last_digit_of_license_plate(self):
         self.assertEqual(7, self.predictor.return_last_digit("PKD-0927"))
+
+    def test_predict_if_pico_and_placa_applies_according_to_time(self):
+        self.assertEqual(False, self.predictor.predict("08H30"))
