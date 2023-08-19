2aeb87b686f3c92cd144aedcc4517ea3421ab0c0
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-08-19 18:24:45 -0500

    test: predict if pico and placa applies based on date with separated method

--- src/predictor.py
+++ src/predictor.py
@@ -7,9 +7,12 @@ class Predictor:
     def return_last_digit(self, license_plate):
         return int(license_plate[-1])
 
-    def predict(self, license_plate, str_date, str_time):
+    def predict(self, str_time):
         time = datetime.datetime.strptime(str_time, "%H:%M").time()
         if (ScheduleTime.TIME_MORNING_START.value <= time <= ScheduleTime.TIME_MORNING_END.value or
                 ScheduleTime.TIME_AFTERNOON_START.value <= time <= ScheduleTime.TIME_AFTERNOON_END.value):
             return False
         return True
+
+    def predict_according_to_date(self, license_plate, str_date):
+        pass
