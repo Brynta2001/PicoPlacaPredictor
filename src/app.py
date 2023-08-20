ea5f8bc1322e33e4980e4c0f84e1f5f6c96659f7
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-08-20 14:49:33 -0500

    fix: solve problem with while loop in app.py

--- src/app.py
+++ src/app.py
@@ -8,36 +8,32 @@ predictor = Predictor()
 
 
 def run():
-    date = ""
-    time = ""
-    license_plate = ""
-
-    option = 1
-    while option == 1:
+    while True:
         date = input("Enter a date in format dd/mm/yyyy\n")
         if not validator.validate_date_format(date):
             print("Invalid date")
-            if int(input("Do you want to enter a date again? 0:Yes, 1:No")) != 0:
+            if int(input("Do you want to enter a date again? 0:Yes, 1:No\n")) != 0:
                 sys.exit()
-        option = 0
+        else:
+            break
 
-    option = 1
-    while option == 1:
+    while True:
         time = input("Enter the time in 24H format hh:mm\n")
         if not validator.validate_time_format(time):
             print("Invalid time")
-            if int(input("Do you want to enter a time again? 0:Yes, 1:No")) != 0:
+            if int(input("Do you want to enter a time again? 0:Yes, 1:No\n")) != 0:
                 sys.exit()
-        option = 0
+        else:
+            break
 
-    option = 1
-    while option == 1:
+    while True:
         license_plate = input("Enter the license plate of the car like the example: PKD-0953\n")
         if not validator.validate_license_plate_format(license_plate):
             print("Invalid license plate")
-            if int(input("Do you want to enter a license plate again? 0:Yes, 1:No")) != 0:
+            if int(input("Do you want to enter a license plate again? 0:Yes, 1:No\n")) != 0:
                 sys.exit()
-        option = 0
+        else:
+            break
 
     if predictor.predict(license_plate, date, time):
         print("The car can be on the road")
