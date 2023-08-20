2cc564ed6601c4b9db54434c104874fb16d760ac
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-08-20 02:23:57 -0500

    feat: implement loops to have a simple menu

--- src/app.py
+++ src/app.py
@@ -6,21 +6,38 @@ from src.validator import Validator
 validator = Validator()
 predictor = Predictor()
 
+
 def run():
-    date = input("Enter a date in format dd/mm/yyyy\n")
-    if not validator.validate_date_format(date):
-        print("Invalid date")
-        sys.exit()
+    date = ""
+    time = ""
+    license_plate = ""
 
-    time = input("Enter the time in 24H format hh:mm\n")
-    if not validator.validate_time_format(time):
-        print("Invalid time")
-        sys.exit()
+    option = 1
+    while option == 1:
+        date = input("Enter a date in format dd/mm/yyyy\n")
+        if not validator.validate_date_format(date):
+            print("Invalid date")
+            if input("Do you want to enter a date again? 0:Yes, 1:No") != 0:
+                sys.exit()
+        option = 0
 
-    license_plate = input("Enter the license plate of the car like the example: PKD-0953\n")
-    if not validator.validate_license_plate_format(license_plate):
-        print("Invalid license plate")
-        sys.exit()
+    option = 1
+    while option == 1:
+        time = input("Enter the time in 24H format hh:mm\n")
+        if not validator.validate_time_format(time):
+            print("Invalid time")
+            if input("Do you want to enter a time again? 0:Yes, 1:No") != 0:
+                sys.exit()
+        option = 0
+
+    option = 1
+    while option == 1:
+        license_plate = input("Enter the license plate of the car like the example: PKD-0953\n")
+        if not validator.validate_license_plate_format(license_plate):
+            print("Invalid license plate")
+            if input("Do you want to enter a license plate again? 0:Yes, 1:No") != 0:
+                sys.exit()
+        option = 0
 
     if predictor.predict(license_plate, date, time):
         print("The car can be on the road")
