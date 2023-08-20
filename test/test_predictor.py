05c271faf3264f3293d3ef1e8f0060f3bb6b56c7
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-08-20 02:31:31 -0500

    test: establish whether is holiday and establish whether is in time range

--- test/test_predictor.py
+++ test/test_predictor.py
@@ -1,4 +1,6 @@
 import unittest
+from datetime import date, time
+
 from src.predictor import Predictor
 
 
@@ -9,6 +11,12 @@ class TestPredictor(unittest.TestCase):
     def test_return_last_digit_of_license_plate(self):
         self.assertEqual(7, self.predictor.return_last_digit("PKD-0927"))
 
+    def test_is_holiday(self):
+        self.assertEqual(True, self.predictor.is_holiday(date(2023, 1, 2)))
+
+    def test_is_in_time_range(self):
+        self.assertEqual(True, self.predictor.is_in_time_range(time(12,30)))
+
     def test_predict_if_pico_and_placa_applies(self):
         self.assertEqual(True,
-                         self.predictor.predict("PKD-0921", "02/01/2023", "08:30"))
+                         self.predictor.predict("PKD-0921", "02/02/2023", "08:30"))
